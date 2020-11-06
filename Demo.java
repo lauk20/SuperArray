4d7e49aeb74aa82060d0191aa6bc42a2f9756001
lauk20 <klau20@stuy.edu>
2020-11-06 12:06:45 -0500

    Added some tests and findOverlap() method. Seems to be working properly.

--- Demo.java
+++ Demo.java
@@ -9,6 +9,20 @@ public class Demo{
     }
   }
 
+  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+    SuperArray newOne = new SuperArray();
+
+    for (int i = 0; i < a.size(); i++){
+      if (b.contains(a.get(i))){
+        newOne.add(a.get(i));
+      }
+    }
+
+    removeDuplicates(newOne);
+
+    return newOne;
+  }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -19,6 +33,41 @@ public class Demo{
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
+
+    System.out.println();
+
+    SuperArray A = new SuperArray();
+    SuperArray B = new SuperArray();
+
+    System.out.println("Overlap here");
+    A.add("9"); A.add("1"); A.add("2"); A.add("2"); A.add("3"); A.add("4");
+    B.add("0"); B.add("4"); B.add("2"); B.add("2"); B.add("9");
+
+    System.out.println(A);
+    System.out.println(B);
+
+    System.out.println(findOverlap(A, B).toString());
+    System.out.println(findOverlap(B, A));
+
+    SuperArray C = new SuperArray();
+    SuperArray D = new SuperArray();
+
+
+    for(int i = 0; i < 10; i++){
+      C.add("1");
+    }
+
+    for(int i = 0; i < 10; i++){
+      D.add("1");
+    }
+
+    System.out.println(findOverlap(C, D));
+
+    System.out.println(C);
+
+    removeDuplicates(C);
+
+    System.out.println(C);
   }
 
 }
